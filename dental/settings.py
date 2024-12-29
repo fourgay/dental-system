2c4cd859104adcbb6ceb44c538e1ca8eb81b8635
sauvole565 <71451891+caophi562005@users.noreply.github.com>
2024-12-29 10:40:23 +0700

    edit cors

--- dental/settings.py
+++ dental/settings.py
@@ -145,6 +145,7 @@ AUTH_USER_MODEL = 'users.Data'
 
 # Cấu hình CORS
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4000",
     "http://127.0.0.1:4000",
