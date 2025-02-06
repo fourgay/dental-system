a2f3206154f4df6cd64bcd7fa6e332375911f6b1
NgoThanhTan1811 <165416753+NgoThanhTan1811@users.noreply.github.com>
2025-02-06 22:31:35 +0700

    fix bug

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated, BasePermission
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
 from .models import Data, Service, Booking
-from .serializers import DataSerializer, ServiceSerializer, BookingSerializer,DataSerializer_admin,DataSerializer_booking
+from .serializers import DataSerializer, ServiceSerializer, BookingSerializer,DataSerializer_admin,DataSerializer_booking,DoctorSerializer
 from .pagination import CustomPagination
 from django.db.models import Q
 from django.db import transaction
